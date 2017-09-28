700d3b6d439e2f84f179d1248eab1ab2624662cb
Jean-Pierre Cuvelliez <jpcuvelliez@gmail.com>
2017-09-28 16:40:54 +0200

    .

--- src/main/java/com/darts/organis/OrganisApplication.java
+++ src/main/java/com/darts/organis/OrganisApplication.java
@@ -7,23 +7,31 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.ServletContextAware;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContext;
 
 @SpringBootApplication
 @ServletComponentScan
-public class OrganisApplication extends SpringBootServletInitializer {
+public class OrganisApplication extends SpringBootServletInitializer implements ServletContextAware {
 
-/*
     @Bean
-    public ServletRegistrationBean testServlet(){
-        final ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new TestServlet(), "/test");
+    public ServletRegistrationBean facesServlet(){
+        final ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new FacesServlet(), "*.xhtml");
+        servletRegistrationBean.setLoadOnStartup(1);
         return servletRegistrationBean;
     }
-*/
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         builder.sources();
         return super.configure(builder);
+    }
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
     }
 
     public static void main(String[] args) {
