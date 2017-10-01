6935e876dcc88de6720a7d489b1b03f109776cda
jpc <jpcuvelliez@gmail.com>
2017-10-01 21:56:39 +0200

    .

--- organis-app/src/app/app.module.ts
+++ organis-app/src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {
+    InputTextModule, ButtonModule, DataTableModule, DialogModule, PanelModule,
+    AccordionModule
+} from 'primeng/primeng';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        InputTextModule,
+        ButtonModule,
+        PanelModule,
+        DataTableModule,
+        DialogModule,
+        AccordionModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
