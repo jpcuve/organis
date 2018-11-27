80c358811f538eb8e876e00e4ba0f718b56b40b4
jpc <jpcuvelliez@gmail.com>
2018-11-27 18:12:48 +0100

    java 11

--- src/main/java/com/darts/organis/OrganisApplication.java
+++ src/main/java/com/darts/organis/OrganisApplication.java
@@ -1,40 +1,12 @@
 package com.darts.organis;
 
-import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.context.ServletContextAware;
-
-import javax.faces.webapp.FacesServlet;
-import javax.servlet.ServletContext;
 
 @SpringBootApplication
 @ServletComponentScan
-public class OrganisApplication extends SpringBootServletInitializer implements ServletContextAware {
-
-    @Bean
-    public ServletRegistrationBean facesServlet(){
-        final ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new FacesServlet(), "/faces/*");
-        servletRegistrationBean.setName("Faces Servlet");
-        servletRegistrationBean.setLoadOnStartup(1);
-        return servletRegistrationBean;
-    }
-
-    @Bean
-    public static CustomScopeConfigurer customScopes(){
-        final CustomScopeConfigurer customScopeConfigurer = new CustomScopeConfigurer();
-        customScopeConfigurer.addScope("view", new ViewScope());
-        return customScopeConfigurer;
-    }
-
-    @Override
-    public void setServletContext(ServletContext servletContext) {
-        servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
-    }
+public class OrganisApplication {
 
     public static void main(String[] args) {
 		SpringApplication.run(OrganisApplication.class, args);
