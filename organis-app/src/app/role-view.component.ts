362ab198f46f8a6e5ebcce084206f8e13ee4d4a5
jpc <jpcuvelliez@gmail.com>
2018-11-28 09:10:27 +0100

    .

--- organis-app/src/app/role-view.component.ts
+++ organis-app/src/app/role-view.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {RemoteService} from "./remote.service";
-import {ActivatedRoute, Params} from "@angular/router";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
     template: `<span>role: {{activatedRoute.snapshot.params['id']}}</span>`
@@ -15,15 +15,6 @@ export class RoleViewComponent implements OnInit {
     }
 
     ngOnInit(): void {
-/*
-        this.activatedRoute.params
-            .switchMap((params: Params) => {
-                console.log('params:', params['id']);
-                return Observable.of('lala');
-            })
-            .subscribe((s: string) => {
-                console.log('string:', s);
-            });
-*/
+      console.log('Role id:', this.activatedRoute.snapshot.paramMap.get('id'));
     }
 }
